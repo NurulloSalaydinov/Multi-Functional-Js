3609fe5c00013745f0ebae14aaad3e288ede0f4b
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-05-25 18:26:01 +0300

    active class added

--- base/base.js
+++ base/base.js
@@ -1,6 +1,4 @@
 // THAT WAS MADE FROM NURLLO SALAYDINOV
-// EXPRESSED BY LIBRARIES
-// COPYRIGHT
 
 // INITIALIZE ATTRIBUTES
 
@@ -12,41 +10,52 @@ function InitializeAttributes() {
             while (i < attr_list.length) {
                 let attr_id = attr_list[i].localName.startsWith("#");
                 let attr_class = attr_list[i].localName.startsWith(".");
-                
+
                 if (attr_class) {
-                    // first way, but first is easy to do
-                    let localName = attr_list[i].localName.replace(/[.]/g, " ")
-                    elem.setAttribute("class", localName)
-                    elem.removeAttribute(attr_list[i].localName)
-                    // second way
-                    // let local_name = attr_list[i].localName
-                    // let joined_class = ''
-                    // for (let j = 0; j < local_name.length; j++) {
-                    //     if (local_name[j] == ".") {
-                    //         joined_class += ' '
-                    //     }
-                    //     else {
-                    //         joined_class += local_name[j]
-                    //     }
-                    // }
-                    // elem.setAttribute("class", joined_class);
-                    // elem.removeAttribute(local_name)
-                }
+                    let localName = attr_list[i].localName.replace(/[.]/g, " ");
+                    elem.setAttribute("class", localName);
+                    elem.removeAttribute(attr_list[i].localName);
+                };
                 if (attr_id) {
-                    let localName = attr_list[i].localName.slice(1, attr_list[i].localName.length)
-                    elem.setAttribute("id", localName)
-                    elem.removeAttribute(attr_list[i].localName)
-                }
+                    let localName = attr_list[i].localName.slice(1, attr_list[i].localName.length);
+                    elem.setAttribute("id", localName);
+                    elem.removeAttribute(attr_list[i].localName);
+                };
                 i++;
-            }
-        }
+            };
+        };
     });
 }
 
-function AddActiveClass(element, className) {
-    elem = eval(`document.querySelector('.${element}')`)
-    // console.log(elem)
-    elem.classList.add(`${className}`)
-}
+function AddClassName(element, className) { // function add class name
+    elem = eval(`document.querySelector('.${element}')`);
+    elem.classList.add(`${className}`);
+};
+
+function AddId(element, idName) { // function add id
+    elem = eval(`document.querySelector('.${element}')`);
+    elem.setAttribute('id', `${idName}`);
+};
+
+function ActivateStyle() {
+    let styleList = {};
+    document.querySelectorAll('*').forEach(elem => {
+        conditional = elem.attributes['style'];
+        if (conditional) {
+            styleList += `${conditional.nodeValue}\n`
+        };
+    });
+    // console.log(styleList);
+    function randomClassName() {
+        let chars = 'qwertyuiopasdfghjklzxcvbnm';
+        let className = '';
+        for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
+            className += chars[Math.floor(Math.random() * chars.length)]
+        }
+        console.log(className);
+    }
+
+    randomClassName();
+};
 
 // console.log("SUCCESSFULLY WORKED !\nNO BUGS FOUND\nIF YOU FIND ANY BUG\nCONTACT SALAYDINOVNURULLO@GMAIL.COM")
