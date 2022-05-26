33a4a79103fe81fd776990b2e144472b184f78c1
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-05-26 10:59:12 +0300

    media styles added

--- base/base.js
+++ base/base.js
@@ -49,12 +49,38 @@ function ActivateStyle() { // function activate style
     var styleObject = {
 
         styleList: [],
+
+        media: {
+            xs: '@media only screen and (max-width: 600px) {\n',
+            sm: '@media only screen and (min-width: 600px) {\n',
+            md: '@media only screen and (min-width: 768px) {\n',
+            lg: '@media only screen and (min-width: 992px) {\n',
+            xl: '@media only screen and (min-width: 1200px) {\n',
+        },
     
-        addElement: function(obj) { 
+        addStyleElement: function(obj) { 
             this.styleList.push(obj)
         },
+        
+        addMediaStyleElement: function(obj, to) {
+            if (to == 'xs') {
+                this.media.xs += obj
+            }
+            else if (to == 'sm') {
+                this.media.sm += obj
+            }
+            else if (to == 'md') {
+                this.media.md += obj
+            }
+            else if (to == 'lg') {
+                this.media.lg += obj
+            }
+            else if (to == 'xl') {
+                this.media.xl += obj
+            }
+        },
 
-        prepareProduction: function() {
+        prepareStyleProduction: function() {
             let preparedStyles = '\n'
             for (let element of this.styleList) {
                 preparedStyles += `.${element.className}{${element.style}}\n`
@@ -64,23 +90,91 @@ function ActivateStyle() { // function activate style
 
         pushToStyleTag: function() {
             let styleTag = document.createElement('style');
-            styleTag.textContent = this.prepareProduction();
+            styleTag.textContent = this.prepareStyleProduction();
+            document.head.appendChild(styleTag);
+        },
+        
+        pushMediaToStyleTag: function() {
+            let styleTag = document.createElement('style');
+                styleTag.textContent += this.media.xs + '\n}\n';
+                styleTag.textContent += this.media.sm + '\n}\n';
+                styleTag.textContent += this.media.md + '\n}\n';
+                styleTag.textContent += this.media.lg + '\n}\n';
+                styleTag.textContent += this.media.xl + '\n}\n';
             document.head.appendChild(styleTag);
         }
 
     };
     
     document.querySelectorAll('*').forEach(elem => {
+        // for only styles
         conditional = elem.attributes['style'];
         if (conditional) {
             randomChar = randomClassName();
-            styleObject.addElement(
+            styleObject.addStyleElement(
                 {'className': randomChar,'style': conditional.nodeValue}
             );
-            styleObject.pushToStyleTag();
-            elem.setAttribute('class', randomChar);
+            elem.classList.add(randomChar);
             elem.removeAttribute('style');
         };
+        // for media styles
+        xs = elem.attributes['xs'];
+        sm = elem.attributes['sm'];
+        md = elem.attributes['md'];
+        lg = elem.attributes['lg'];
+        xl = elem.attributes['xl'];
+
+        if (xs) {
+            randomClass = randomClassName();
+            styleObject.addMediaStyleElement(
+                `.${randomClass} {
+                    ${xs.nodeValue}
+                }`, 'xs'
+            )
+            elem.classList.add(randomClass)
+            elem.removeAttribute('xs')
+        }
+        if (sm) {
+            randomClass = randomClassName();
+            styleObject.addMediaStyleElement(
+                `.${randomClass} {
+                    ${sm.nodeValue}
+                }`, 'sm'
+            )
+            elem.classList.add(randomClass)
+            elem.removeAttribute('sm')
+        }
+        if (md) {
+            randomClass = randomClassName();
+            styleObject.addMediaStyleElement(
+                `.${randomClass} {
+                    ${md.nodeValue}
+                }`, 'md'
+            )
+            elem.classList.add(randomClass)
+            elem.removeAttribute('md')
+        }
+        if (lg) {
+            randomClass = randomClassName();
+            styleObject.addMediaStyleElement(
+                `.${randomClass} {
+                    ${lg.nodeValue}
+                }`, 'lg'
+            )
+            elem.classList.add(randomClass)
+            elem.removeAttribute('lg')
+        }
+        if (xl) {
+            randomClass = randomClassName();
+            styleObject.addMediaStyleElement(
+                `.${randomClass} {
+                    ${xl.nodeValue}
+                }`, 'xl'
+            )
+            elem.classList.add(randomClass)
+            elem.removeAttribute('xl')
+        }
+
     });
 
     function randomClassName() {
@@ -90,7 +184,10 @@ function ActivateStyle() { // function activate style
             className += chars[Math.floor(Math.random() * chars.length)]
         }
         return className
-    }
+    };
+
+    styleObject.pushToStyleTag();
+    styleObject.pushMediaToStyleTag();
 
 };
 
